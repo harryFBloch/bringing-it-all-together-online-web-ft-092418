31b8462ac08eaeccbcf2377580f933d01225e8ab
Harry Bloch <harryfbloch@gmail.com>
2018-10-25 21:03:25 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,60 @@
+class Dog
+attr_accessor :name, :breed, :id
+
+  def initialize(name: nil, breed: nil, id:  nil)
+    self.name = name 
+    self.breed = breed
+    self.id = id
+  end
+  
+  def self.create_table
+    DB[:conn].execute("CREATE TABLE dogs (id PRIMARY KEY, name TEXT, breed TEXT)")
+  end
+  
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs")
+  end
+  
+  def save
+    if self.id 
+      
+    else 
+      DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?,?)",self.name,self.breed)
+      #binding.pry
+      self.id = DB[:conn].execute("SELECT id FROM dogs WHERE name = ?",self.name)[0][0]
+    end
+    self
+  end
+  
+  def self.create(hash)
+    #binding.pry
+    dog = Dog.new()
+    hash.each {|key, value|
+      dog.send("#{key}=",value)
+    }
+    dog.save
+  end
+  
+  def self.find_by_id(id)
+    DB[:conn].results_as_hash = true
+    info = DB[:conn].execute(
+      "SELECT * FROM dogs WHERE id = ?",id).first
+    #binding.pry
+    dog = Dog.new(name: info["name"], breed: info["breed"], id: info["id"])
+    dog.save
+  end
+  
+  def self.find_or_create_by(hash)
+    
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?",hash[:name], hash[:breed])
+    #binding.pry
+    if !dog.empty?
+      binding.pry
+      self.find_by_id(dog[:id])
+    else
+      #binding.pry
+      dog = Dog.create(hash)
+    end
+  end
+  
+end
